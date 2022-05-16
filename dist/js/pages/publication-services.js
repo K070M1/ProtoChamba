8a18d2997b3cb702ba98e5d2be0db9983fef84ac
Jesus <1142396@senati.pe>
2022-05-16 08:18:07 +0100

    Ningun cambio

--- dist/js/pages/publication-services.js
+++ dist/js/pages/publication-services.js
@@ -530,7 +530,8 @@ $("#btn-modify-publication").click(function(){
           contentType: false,
           processData: false,
           cache: false,
-          success: function(result){            
+          success: function(result){  
+            console.log(result);          
             if(result == ""){
               idtrabajo = -1;
               clearFormPublication();
